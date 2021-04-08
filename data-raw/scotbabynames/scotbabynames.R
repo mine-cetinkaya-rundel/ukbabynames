af739245c29f09f624863cb4ddf9fcd78f26a839
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-04-08 15:33:57 +0100

    Clean scotbabynames

--- data-raw/scotbabynames/scotbabynames.R
+++ data-raw/scotbabynames/scotbabynames.R
@@ -1,6 +1,28 @@
-# construct the `babynames` dataset from nrscotland.gov.
+# download the `scotbabynames` dataset from nrscotland.gov --------------------
 
-dir.create("data-raw/f+m-scotland")
-if(!file.exists("data-raw/f+m-scotland/1974-2020.csv"))
-download.file("https://www.nrscotland.gov.uk/files//statistics/babies-names/20/babies-first-names-all-names-all-years.csv",
-              "data-raw/f+m-scotland/1974-2020.csv", mode = "wb")
+dir.create("data-raw/scotbabynames/f-m")
+
+if(!file.exists("data-raw/scotbabynames/f-m/1974-2020.csv")){
+  download.file("https://www.nrscotland.gov.uk/files//statistics/babies-names/20/babies-first-names-all-names-all-years.csv",
+                "data-raw/scotbabynames/f-m/1974-2020.csv", mode = "wb")
+}
+
+# import scot data -------------------------------------------------------------
+
+scotbabynames <- readr::read_csv("data-raw/scotbabynames/f-m/1974-2020.csv")
+
+# clean scot data --------------------------------------------------------------
+
+scotbabynames %>%
+  rename(
+    year = yr,
+    name = FirstForename,
+    n = number
+  ) %>%
+  subset(select = c(year, sex, name, n, rank)) %>%
+  mutate(nation = "Scotland")
+
+# export -----------------------------------------------------------------------
+
+readr::write_csv(scotbabynames, "data-raw/scotbabynames/scotbabynames.csv")
+usethis::use_data(scotbabynames, overwrite = TRUE)
