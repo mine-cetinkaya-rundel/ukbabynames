69ae8e9dfa48ff67833e4bfa5f83228035fca611
ngoguened <s2013017@ed.ac.uk>
2021-04-13 20:20:32 +0000

    update file

--- R/ukbabynames.R
+++ R/ukbabynames.R
@@ -1,15 +1,16 @@
 #' @rdname ukbabynames
 #' @title UK baby names
-#' @description Full baby name 1996-2019 for the United Kingdom from the Office of National Statistics
+#' @description Full baby name 1974-2020 for the United Kingdom from the various sites.
 #'
 #' @format 
-#'   A data frame with 254889 observations on 5 variables.
+#'   A data frame with 550587 observations on 6 variables.
 #'   \tabular{rll}{
 #'     [, 1] \tab year  \tab A year (1996-2019) \cr
 #'     [, 2] \tab sex   \tab \dQuote{M} = Male; \dQuote{F} = Female \cr
 #'     [, 3] \tab name  \tab A name \cr
 #'     [, 4] \tab n     \tab Count of name within year and sex \cr
 #'     [, 5] \tab rank  \tab Rank of name within year and sex \cr
+#'     [, 5] \tab nation  \tab Nation of data source \cr
 #'   }
 #' 
 #' @details
@@ -18,5 +19,7 @@
 #' @source
 #' Office of National Statistics. 2021. \href{https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/datasets/babynamesenglandandwalesbabynamesstatisticsgirls}{\dQuote{Baby Names Statistics Girls}}.
 #' Office of National Statistics. 2021. \href{https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/datasets/babynamesenglandandwalesbabynamesstatisticsboys}{\dQuote{Baby Names Statistics Boys}}.
+#' Northern Ireland Statistics and Research Agency. 2021. \href{https://www.nisra.gov.uk/publications/baby-names-2018}{\dQuote{Baby Names Statistics Boys and Girls}}.
+#' National Records of Scotland. 2021. \href{https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/vital-events/names/babies-first-names/babies-first-names-2020}{\dQuote{Baby Names Statistics Boys and Girls}}.
 #' @seealso \code{\link{rankings}}
 "ukbabynames"
