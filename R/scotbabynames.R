dd809985e4a41ab7878104fa0cec618349a4c44a
ngoguened <s2013017@ed.ac.uk>
2021-04-13 20:10:03 +0000

    Updated file.

--- R/scotbabynames.R
+++ R/scotbabynames.R
@@ -1,13 +1,13 @@
 #' Scotland baby names
 #'
-#' Full baby name data from 1996 to 2019 for England and Wales
+#' Full baby name data from 1974 to 2020 for Scotland
 #' from the Office of National Statistics.
 #'
 #' @name scotbabynames
 #' @docType data
-#' @format A data frame with 281667 observations on 5 variables.
+#' @format A data frame with 248420 observations on 5 variables.
 #' \describe{
-#'   \item{year}{A year (1996-2019)}
+#'   \item{year}{A year (1974-2020)}
 #'   \item{sex}{Sex, M = Male and F = Female.}
 #'   \item{name}{A name.}
 #'   \item{n}{Count of name within year and sex.}
@@ -15,10 +15,8 @@
 #'   \item{nation}{Nation of data source.}
 #' }
 #' @details
-#' The data are released by the Office of National Statistics under the \href{http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/}{Open Government License v3.0}.
+#' The data are released by the National Records of Scotland under the \href{http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/}{Open Government License v3.0}.
 #' 
 #' @source
-#' Office of National Statistics. 2021. \href{https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/datasets/babynamesenglandandwalesbabynamesstatisticsgirls}{\dQuote{Baby Names Statistics Girls}}.
-#' Office of National Statistics. 2021. \href{https://www.ons.gov.uk/peoplepopulationandcommunity/birthsdeathsandmarriages/livebirths/datasets/babynamesenglandandwalesbabynamesstatisticsboys}{\dQuote{Baby Names Statistics Boys}}.
-#' @seealso \code{\link{rankings}}
+#' National Records of Scotland. 2021. \href{https://www.nrscotland.gov.uk/statistics-and-data/statistics/statistics-by-theme/vital-events/names/babies-first-names/babies-first-names-2020}{\dQuote{Baby Names Statistics Boys and Girls}}.
 "scotbabynames"
